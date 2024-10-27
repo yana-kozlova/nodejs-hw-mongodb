a634b8acab07f11c7af47923823ff2d999f2f6b4
Yana Kozlova <jasenkozlova@gmail.com>
2024-10-27 23:16:58 +0200

    basic server setup

--- src/server.js
+++ src/server.js
@@ -1,3 +1,40 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
 
-const app = express();
+export const startServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty'
+    }
+  })
+
+  app.use(logger);
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Start project'
+    })
+  })
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`
+    })
+  })
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message
+    });
+  });
+
+  const port = Number(env('PORT', 3000));
+
+  app.listen(port, ()=> console.log('Server running on 3000 PORT'))
+}
