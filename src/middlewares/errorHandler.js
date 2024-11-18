7d8f1840a23b82e5eda2fd473cc3670ca707a3ce
Yana Kozlova <jasenkozlova@gmail.com>
2024-11-18 19:59:55 +0200

    fix: added sort, filter

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 export const errorHandler = (error, req, res, next) => {
   const status = error.status || 500;
-  const message = error.name ? error.name : "Error";
+  const message = error.name || "Error";
   const detailedMessage = error.message || 'Server error';
 
   res.status(status).json({
