663a3a7fb51377e1a8fcac919d848695e0c0f80d
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-06 14:51:33 +0200

    feat: allow cookie and http://localhost:3000

--- src/server.js
+++ src/server.js
@@ -14,7 +14,11 @@ import router from './routers/index.js';
 export const startServer = () => {
   const app = express();
 
-  app.use(cors());
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Разрешаем куки
+  }));
+
   app.use(express.json());
   app.use(cookieParser());
 
